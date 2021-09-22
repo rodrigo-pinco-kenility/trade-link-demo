1d4447266a465bcabb82f28a7a5b155adb470c88
Rodrigo Pinco <rodrigo.pinco@kenility.com>
2021-09-22 12:21:39 -0300

    Added filesize label

--- pages/step-one.js
+++ pages/step-one.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import VideoRecorder from 'react-video-recorder';
 
 export default function StepOne() {
-  let videoRecord;
+  const [videoRecord, setVideoRecord] = useState();
 
   return (
     <>
       <h2>this is step two, record a video</h2>
-
-      <VideoRecorder
-        constraints={{
-          audio: true,
-          video: true,
-        }}
-        isFlipped={false}
-        showReplayControls
-        onRecordingComplete={(videoBlob) => {
-          // Do something with the video...
-          videoRecord = videoBlob;
-          console.log('videoBlob', videoBlob);
-        }}
-      />
+      <div style={{ height: '80vh' }}>
+        <VideoRecorder
+          constraints={{
+            audio: true,
+            video: true,
+          }}
+          isFlipped={false}
+          showReplayControls
+          onRecordingComplete={(videoBlob) => {
+            // Do something with the video...
+            setVideoRecord(videoBlob);
+            console.log('videoBlob', videoRecord);
+          }}
+        />
+      </div>
 
       <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-        <span>The file size is: {videoRecord.size}</span>
+        <span>The file size is: {videoRecord ? (videoRecord.size / (1024 * 1024)).toFixed(2) + 'Mb' : 'Not available'}</span>
       </div>
     </>
   );
