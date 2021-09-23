069d1609b55be34518b0aad763db229324ba7c98
Rodrigo Pinco <rodrigo.pinco@kenility.com>
2021-09-23 13:04:35 -0300

    replaced library, record poc v2

--- pages/step-one.js
+++ pages/step-one.js
@@ -1,30 +1,83 @@
+import { Button } from '@mui/material';
 import React, { useState } from 'react';
-import VideoRecorder from 'react-video-recorder';
+import Timer from 'react-compound-timer/build';
+import Webcam from 'react-webcam';
+
+import styles from './step-one.module.css';
 
 export default function StepOne() {
-  const [videoRecord, setVideoRecord] = useState();
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+  };
+
+  const timer = React.useRef(null);
+
+  const webcamRef = React.useRef(null);
+  const mediaRecorderRef = React.useRef(null);
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
+
+  const handleStartCaptureClick = React.useCallback(() => {
+    timer.current.start();
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: 'video/webm',
+    });
+    mediaRecorderRef.current.addEventListener('dataavailable', handleDataAvailable);
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef]);
+
+  const handleDataAvailable = ({ data }) => {
+    if (data.size > 0) {
+      setRecordedChunks((prev) => prev.concat(data));
+    }
+  };
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+    timer.current.stop();
+  }, [mediaRecorderRef, setCapturing]);
+
+  const calculateTotalSize = () => {
+    let size = 0;
+    recordedChunks.forEach((chunk) => {
+      size += chunk.size;
+    });
+
+    size = (size / (1024 * 1024)).toFixed(2);
+    return size + 'Mb';
+  };
 
   return (
     <>
       <h2>this is step two, record a video</h2>
-      <div style={{ height: '80vh' }}>
-        <VideoRecorder
-          constraints={{
-            audio: true,
-            video: true,
-          }}
-          isFlipped={false}
-          showReplayControls
-          onRecordingComplete={(videoBlob) => {
-            // Do something with the video...
-            setVideoRecord(videoBlob);
-            console.log('videoBlob', videoRecord);
-          }}
-        />
-      </div>
+      <div className={styles.controls}>
+        <Webcam audio={true} ref={webcamRef} />
+        {capturing ? (
+          <Button className={styles.record_button} variant="contained" onClick={handleStopCaptureClick}>
+            Stop Capture
+          </Button>
+        ) : (
+          <Button className={styles.record_button} variant="contained" onClick={handleStartCaptureClick}>
+            Start Capture
+          </Button>
+        )}
+        <Timer ref={timer} startImmediately={false}>
+          {({ start, pause, stop, reset }) => (
+            <React.Fragment>
+              <div>
+                <Timer.Minutes />:
+                <Timer.Seconds />
+              </div>
+            </React.Fragment>
+          )}
+        </Timer>
 
-      <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
-        <span>The file size is: {videoRecord ? (videoRecord.size / (1024 * 1024)).toFixed(2) + 'Mb' : 'Not available'}</span>
+        <div style={{ display: 'flex', justifyContent: 'center', padding: '20px' }}>
+          <span>The file size is: {recordedChunks ? calculateTotalSize() : 'Not available'}</span>
+        </div>
       </div>
     </>
   );
